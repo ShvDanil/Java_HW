fb391e11bc763f60e28ad198083dacc1ccedbe41
ShvDanil <dishvetsov@edu.hse.ru>
2023-01-06 14:24:52 +0400

    Adding entry point of the program form Handler class (which regulates the program flow)

--- HW2_LoopDependency/src/Main.java
+++ HW2_LoopDependency/src/Main.java
@@ -1,5 +1,12 @@
+/**
+ * The main class of the project, which has the executable point which starts the program.
+ */
 public class Main {
+    /**
+     * Entry point of the program which starts program execution.
+     */
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Handler handler = new Handler();
+        handler.execute();
     }
 }
