f748bef716a42491c144ef0fcc3e1a6c47ab3186
ShvDanil <dishvetsov@edu.hse.ru>
2023-01-06 14:36:22 +0400

    Adding class which executes the print of logs to console

---
+++ HW2_LoopDependency/src/ConsoleLogger.java
@@ -0,0 +1,14 @@
+/**
+ * Logger class which prints logs to the console.
+ */
+public final class ConsoleLogger {
+
+    /**
+     * Prints the console log from handler to the user's console.
+     * @param log String representation of log from enum.
+     */
+    public static void log(ConsoleLogs log) {
+        System.out.print(log);
+    }
+
+}
