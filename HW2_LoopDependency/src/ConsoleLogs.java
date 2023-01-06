94edab7dbf2522d8acb5e7c1b923d99f06cfe49e
ShvDanil <dishvetsov@edu.hse.ru>
2023-01-06 14:37:27 +0400

    Adding enum which contains logs to the console in string representation + instructions&lines_separator logs

---
+++ HW2_LoopDependency/src/ConsoleLogs.java
@@ -0,0 +1,41 @@
+/**
+ * Logs to console from handler concerning determined cases based on common program flow.
+ */
+public enum ConsoleLogs {
+
+    /**
+     * Separates lines with '—' sign for external attractiveness of the console output.
+     */
+    LINES_SEPARATOR {
+        @Override
+        public String toString() {
+            return """
+                — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — —
+
+                """;
+        }
+    },
+
+    /**
+     * Prints the instructions to the console.
+     */
+    INSTRUCTIONS {
+        @Override
+        public String toString() {
+            return """
+            [Introduction]
+            Hello! Glad to see you in program "Loop Dependency Checker".
+            
+            [Instructions]
+            The instructions of that program are extremely straight.
+            
+            [Condition] -> you will be asked to enter a root directory and program has to define whether there\040
+            exists a loop dependency in text files of directory or not.
+            
+            [Result] -> if loop dependency is indicated, you will be notified, otherwise, it doesn't exist in directory.
+            
+            """;
+        }
+    },
+
+}
