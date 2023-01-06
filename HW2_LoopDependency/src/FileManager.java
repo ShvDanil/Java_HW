5bf88a4192e55a27d95afde1615f81677b61dac1
ShvDanil <dishvetsov@edu.hse.ru>
2023-01-06 15:46:39 +0400

    Adding loop dependency method check

--- HW2_LoopDependency/src/FileManager.java
+++ HW2_LoopDependency/src/FileManager.java
@@ -1,6 +1,12 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Scanner;
+import java.util.*;
+
+import static java.lang.System.exit;
 
 /**
  * File manager class which handles all operations based on dependency check and work with files.
@@ -9,6 +15,20 @@ public final class FileManager {
 
     // Path to the root directory.
     private Path rootDirectoryPath;
+
+    // Files which were attended during the traversal.
+    private final Set<Path> attendedFiles;
+
+    // Consequently united files.
+    private final List<Path> unitedFiles;
+
+    /**
+     * Constructor which initializes the fields with empty collections to avoid null.
+     */
+    public FileManager() {
+        attendedFiles = new HashSet<>();
+        unitedFiles = new ArrayList<>();
+    }
 
     /**
      * Function which gets and validates the absolute path of root directory.
@@ -30,6 +50,48 @@ public final class FileManager {
     }
 
     /**
+     * Accessor for the root directory absolute path.
+     * @return Absolute path to root directory.
+     */
+    public Path getRootDirectoryPath() {
+        return rootDirectoryPath;
+    }
+
+    /**
+     * Function which traverses all files and directories to extract the existence of loop dependency.
+     * @param absolutePath Absolute path to directory or file.
+     */
+    public void traverseDirectory(Path absolutePath) {
+        // Check for existence of directory with received absolute path.
+        if (!Files.exists(absolutePath)) {
+            ConsoleLogger.log(FileManagerLogs.NOTIFY_DIRECTORY_NOT_EXIST);
+            return;
+        }
+
+        // Getting list of all directories and files.
+        File[] files = (new File(absolutePath.toUri())).listFiles();
+        if (files == null) {
+            return;
+        }
+
+        for (var file : files) {
+            // Walk through files.
+            if (file.isFile()) {
+                // Read file data to find other dependencies.
+                readFileData(file);
+            }
+        }
+
+        for (var file : files) {
+            // Walk through directories.
+            if (file.isDirectory()) {
+                // Traverse directory within a recursive call.
+                traverseDirectory(file.toPath());
+            }
+        }
+    }
+
+    /**
      * Function which scans the entered absolute path for the directory.
      * @return Path to the directory.
      */
@@ -37,4 +99,49 @@ public final class FileManager {
         Scanner scanner = new Scanner(System.in);
         return Path.of(scanner.nextLine());
     }
+
+    /**
+     * Function which reads file and searches for the dependency existence.
+     * @param file Input file with data and maybe dependencies to other files.
+     */
+    private void readFileData(File file) {
+        if (!attendedFiles.contains(file.toPath())) {
+
+            // Check existence of file.
+            if (!Files.exists(file.toPath())) {
+                ConsoleLogger.log(FileManagerLogs.NOTIFY_FILE_NOT_EXIST);
+                System.out.printf("[%s]\n", file.getName());
+                return;
+            }
+
+            // Add path to attended files set.
+            attendedFiles.add(file.toPath());
+
+            // Read file data.
+            try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file.getPath()))) {
+
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+
+                    // Check for dependencies existences.
+                    if (line.startsWith("require ")) {
+                        Path newPathName = rootDirectoryPath.resolve(line.substring(9, line.length() - 1));
+                        // Recursively read data from dependency file.
+                        readFileData(new File(newPathName.toUri()));
+                    }
+                }
+            } catch (IOException e) {
+                ConsoleLogger.log(FileManagerLogs.NOTIFY_SYSTEM_ERROR_IOEXCEPTION_DETECTED);
+            }
+
+            // Add current file to united files only after all dependencies check.
+            unitedFiles.add(file.toPath());
+
+        } else if (!unitedFiles.contains(file.toPath())) {
+            ConsoleLogger.log(FileManagerLogs.NOTIFY_LOOP_DEPENDENCY_FOUND);
+
+            exit(0);
+        }
+    }
+
 }
