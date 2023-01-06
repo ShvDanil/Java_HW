5dbffb24af582954cbb585d58b65120b9c2b34de
ShvDanil <dishvetsov@edu.hse.ru>
2023-01-06 14:23:23 +0400

    Adding handler class which regulates the main flow of program

---
+++ HW2_LoopDependency/src/Handler.java
@@ -0,0 +1,11 @@
+/**
+ * The class which handles the main flow of the program and invokes necessary functions.
+ */
+public class Handler {
+    /**
+     * Function which regulate all program flow calling other functions.
+     */
+    public void execute() {
+
+    }
+}
